c48171f2f734d22b6753606c9df57681ba6570b0
parthergk <gauravkumar81464@gmail.com>
2025-04-26 21:43:55 +0530

    db bug

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,9 @@
-import { prismaClient } from "@repo/db/client";
+import prismaClient from "@repo/db/client";
 
 export default async function Home() {
   const user = await prismaClient.user.findMany();
   console.log("user", user);
-  
+  prismaClient
   return (
     <div>Hello world</div>
   );
